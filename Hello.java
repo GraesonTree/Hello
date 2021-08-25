b301a3ef9323b2abf2fde9ba69512f9f45fd0ba0
GraesonTyree <89479838+GraesonTyree@users.noreply.github.com>
2021-08-25 10:35:24 -0400

    Add First Actual Code

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*
+*Hello
+
+*Graeson Tyree
+*8/24/2021
+*CMSC255
+*/
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Graeson Tyree");
+		System.out.println("Hello");
+		System.out.println("Print out all given prompts");
+		System.out.println("8/24/2021");
+		System.out.println("CMSC255");
+		System.out.println();
+	}
+}
